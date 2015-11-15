d9be362a612602d48693b15ec80421649083b670
Bharath Attaluri <bharathattaluri7@gmail.com>
2015-11-15 01:42:12 -0600

    Included Angular JS in Sign in and Sign up pages

---
+++ BitHorizonWeb/WebContent/javascript/app/controllers.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pradyumnad on 11/15/15.
+ */
+angular.module("app", [])
+
+    .controller("SignInCtrl", function () {
+
+    })
+    .controller("SignUpCtrl", function () {
+    });
